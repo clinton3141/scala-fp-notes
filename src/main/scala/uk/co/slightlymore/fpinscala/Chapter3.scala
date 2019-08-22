4c3ecbab78d0653bce0bd37a372d1ff307ec2afc
Clinton Montague <clinton@serelay.com>
2019-08-22 18:51:16 +0100

    Exercise 3.1

--- src/main/scala/uk/co/slightlymore/fpinscala/Chapter3.scala
+++ src/main/scala/uk/co/slightlymore/fpinscala/Chapter3.scala
@@ -15,5 +15,15 @@ object Chapter3 {
     val nilList = Nil
     println(List.sum(nilList))
     println(List.product(nilList))
+    
+    // ex 3.1: what's the output?
+    val x = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x // no match - missing 3
+      case Nil => 42 // no match - not nil
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y // match - x = 1, y = 2, _ = Cons(5, Nil)
+      case Cons(h, t) => h + List.sum(t) // no match - already matched. But would do so if no previous match
+      case _ => 101 // no match - already matched. But would do so if no previous match
+    }
+    println(x)
   }
 }
