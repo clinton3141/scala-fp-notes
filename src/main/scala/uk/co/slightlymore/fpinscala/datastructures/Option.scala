9721c4fde9f6172aaefe6d13ff5fda246d8fca0a
Clinton Montague <clinton@serelay.com>
2019-09-23 21:07:10 +0100

    Ex 4.3

--- src/main/scala/uk/co/slightlymore/fpinscala/datastructures/Option.scala
+++ src/main/scala/uk/co/slightlymore/fpinscala/datastructures/Option.scala
@@ -16,6 +16,15 @@ sealed trait Option[+A] {
   def orElse[B >: A](ob: => Option[B]): Option[B] = this.map(x => Some(x)).getOrElse(ob)
 
   def filter(f: A => Boolean): Option[A] = this.flatMap(x => if (f(x)) Some(x) else None)
+  
+  // first attempt
+  def map2_using_match[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+    case (Some(aa), Some(bb)) => Some(f(aa, bb))
+    case _ => None
+  }
+  
+  // second attempt - presumably better (more FP) because it's using flatMap? 
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a.flatMap(a_ => b.map(b_ => f(a_, b_)))
 }
 case class Some[A](value: A) extends Option[A]
 case object None extends Option[Nothing]
