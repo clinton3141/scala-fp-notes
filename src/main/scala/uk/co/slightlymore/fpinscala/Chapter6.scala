9d6d90c140f84afb0003456f351a9f85ccd67404
Clinton Montague <clinton@serelay.com>
2019-10-13 16:29:33 +0100

    C6: Ex 6.1 - 6.4

---
+++ src/main/scala/uk/co/slightlymore/fpinscala/Chapter6.scala
@@ -0,0 +1,9 @@
+package uk.co.slightlymore.fpinscala
+
+import uk.co.slightlymore.fpinscala.state.RNG
+
+object Chapter6 {
+  def main(args: Array[String]): Unit = {
+    val rng = RNG.Simple(42)    
+  }
+}
---
+++ src/main/scala/uk/co/slightlymore/fpinscala/state/SimpleRNG.scala
@@ -0,0 +1,59 @@
+package uk.co.slightlymore.fpinscala.state
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = Simple(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }    
+  }
+
+  // Ex 6.1
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (n, r) = rng.nextInt
+    if (n == Int.MinValue) (Int.MaxValue, r)
+    else if (n < 0) (-n, r)
+    else (n, r)
+  }
+  
+  // Ex 6.2
+  def double(rng: RNG): (Double, RNG) = {
+    val (n, r) = rng.nextInt
+    (1D / n.toDouble, r)
+  }
+  
+  // Ex 6.3
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
+  
+  // Ex 6.4
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = count match {
+    case 0 => (List[Int](), rng)
+    case n => {
+      val (i, r1) = rng.nextInt
+      val (is, r2) = ints(n - 1)(r1)
+      ((i :: is), r2)
+    }
+  }
+}
