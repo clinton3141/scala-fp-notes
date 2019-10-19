0f133e7b6f95c829ea791b6e9b876902e3823a21
Clinton Montague <clinton@serelay.com>
2019-10-19 19:34:32 +0100

    Add get and set to State

--- src/main/scala/uk/co/slightlymore/fpinscala/state/State.scala
+++ src/main/scala/uk/co/slightlymore/fpinscala/state/State.scala
@@ -13,6 +13,10 @@ case class State[S, +A](run: S => (A, S)) {
     
   def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
     flatMap(a => sb.map(b => f(a, b)))
+    
+  def get[S]: State[S, S] = State(s => (s, s))
+  
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
 }
 
 object State {
