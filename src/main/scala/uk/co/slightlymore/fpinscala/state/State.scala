052833f939f1bd364a4425b5f65c8ec1e1dc3733
Clinton Montague <clinton@serelay.com>
2019-10-25 18:42:46 +0100

    Add State.get, State.set, State.modify

--- src/main/scala/uk/co/slightlymore/fpinscala/state/State.scala
+++ src/main/scala/uk/co/slightlymore/fpinscala/state/State.scala
@@ -13,10 +13,6 @@ case class State[S, +A](run: S => (A, S)) {
     
   def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
     flatMap(a => sb.map(b => f(a, b)))
-    
-  def get[S]: State[S, S] = State(s => (s, s))
-  
-  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
 }
 
 object State {
@@ -24,4 +20,13 @@ object State {
   
   def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] = 
     fs.foldRight(unit[S, List[A]](List[A]()))((s, acc) => s.map2(acc)(_ :: _))
+    
+  def get[S]: State[S, S] = State(s => (s, s))
+  
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+  
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
 }
